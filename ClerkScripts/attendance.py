af1af257ff25dd49fd8377d50ba9528066b5eedc
Spencer T. Parkin <spencertparkin@gmail.com>
2024-12-08 10:56:50 -0700

    Update to show semi-annual membership audit should be done.

--- ClerkScripts/attendance.py
+++ ClerkScripts/attendance.py
@@ -1,12 +1,13 @@
 # attendance.py -- Quick script to calculate attendance taken during Sacrament meeting.
 
 count_list = [
-    8, 14, 5, 10, 10, 17, 13, 12, 7, 12, 7, 12, 7, 19, 2, 5, 2, 10, 6, 8, 5, 5
+    5, 10, 16, 13, 14, 16, 11, 9, 8, 9, 7, 5, 1, 13,
+    2, 2, 3, 3, 4, 3, 9, 10, 11, 6, 4, 2
 ]
 
 if __name__ == '__main__':
 
-    zoom = 13
+    zoom = 11
     zoom_factor = 1.5
 
     attendance_count = int(round(float(sum(count_list)) + float(zoom) * zoom_factor))
