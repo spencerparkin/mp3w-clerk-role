0f1cbd3bfb902e396e66648b9d8b0beef9e40df8
Spencer T. Parkin <spencertparkin@gmail.com>
2024-12-29 14:00:25 -0700

    Add some info I didn't know.

--- ClerkScripts/attendance.py
+++ ClerkScripts/attendance.py
@@ -1,13 +1,13 @@
 # attendance.py -- Quick script to calculate attendance taken during Sacrament meeting.
 
 count_list = [
-    5, 10, 16, 13, 14, 16, 11, 9, 8, 9, 7, 5, 1, 13,
-    2, 2, 3, 3, 4, 3, 9, 10, 11, 6, 4, 2
+    5, 13, 11, 12, 10, 9, 6, 17, 6, 9, 6, 17,
+    4, 8, 9, 10, 5, 7, 7, 6
 ]
 
 if __name__ == '__main__':
 
-    zoom = 11
+    zoom = 16
     zoom_factor = 1.5
 
     attendance_count = int(round(float(sum(count_list)) + float(zoom) * zoom_factor))
