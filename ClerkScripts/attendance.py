ea4f58c6f0c7ddb51a36a71b39d8f386aeebc11a
Spencer Parkin <spencer.parkin@proton.me>
2026-02-22 21:28:02 -0700

    Update prayer record.

--- ClerkScripts/attendance.py
+++ ClerkScripts/attendance.py
@@ -1,8 +1,8 @@
 # attendance.py -- Quick script to calculate attendance taken during Sacrament meeting.
 
 count_list = [
-    10, 8, 12, 12, 11, 3, 14, 16,
-    12, 8, 8, 19, 4, 4, 2, 3, 2, 7, 1, 11, 9, 6, 7
+    9, 10, 15, 9, 12, 8, 10, 10, 7, 5, 7, 15,
+    4, 1, 1, 2, 8, 3, 8, 5, 7, 5, 7
 ]
 
 if __name__ == '__main__':
